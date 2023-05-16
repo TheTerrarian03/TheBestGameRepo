b63c1412ec43fb0954402cd7396423d1ef00bbf7
ieguras <0510020@mlsd.org>
2023-05-16 09:41:10 -0700

    made the readme

---
+++ Assets/README.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class README : MonoBehaviour
+{
+/*
+  KEEP THE ASSETS FOLDER ORGANIZED OR SO HELP ME I WILL COME TO YOUR HOUSE AND:
+    - REORIENT YOUR TOILET PAPER
+    - WARM YOUR PILLOW
+    - UNPLUG YOUR PHONE AT NIGHT
+    - MAKE YOUR WATER TASTE BAD
+    - UNSEASON YOUR CHICKEN
+    - COOK YOUR STEAK WELL DONE
+ FROM,
+    XAVIER
+*/
+}
