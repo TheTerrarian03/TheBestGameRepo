f76cc183d0becc9c941afc9531896f9767e53d96
Antonio <0508626@mlsd.org>
2023-05-22 09:49:35 -0700

    More Pancake
    Pancake animation can play, not programmed to be played when you click on it yet.

---
+++ Assets/Testing/Test Assets/PancakeFlip.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PancakeFlip : MonoBehaviour
+{
+
+    public bool click = false;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //animator = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
