adc56d84502a0f098a68ee36d2c42563cff7b6a3
ieguras <0510020@mlsd.org>
2023-05-23 09:23:08 -0700

    IT WORRRRKS LMAO

--- Assets/C# Scripts/playermovement.cs
+++ Assets/C# Scripts/playermovement.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class playermovement : MonoBehaviour
 {
+    public float speed;
+    public float grounddistance;
+
+    public LayerMask terrainLayer;
+    public Rigidbody rb;
+    public SpriteRenderer sr;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+        Vector3 castPos = transform.position;
+        castPos.y += 1;
+        if(Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, terrainLayer))
+        {
+            if(hit.collider != null)
+            {
+                Vector3 movePos = transform.position;
+                movePos.y = hit.point.y + grounddistance;
+                transform.position = movePos;
+            }
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 moveDirection = new Vector3(x, 0, y);
+        rb.velocity = moveDirection * speed;
+
+        if(x != 0 && x > 0)
+        {
+            sr.flipX = true;
+        }
+        else if(x != 0 && x > 0)
+        {
+            sr.flipX = true;
+        }
     }
 }
