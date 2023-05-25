7e1d44dda975d778c915d4ec14d3e132e12b4d8e
Antonio <0508626@mlsd.org>
2023-05-25 08:57:33 -0700

    Pancake minigame progress.
    Small update, nothing to say. It's still W.I.P.

--- Assets/Testing/Test Assets/PancakeFlip.cs
+++ Assets/Testing/Test Assets/PancakeFlip.cs
@@ -11,12 +11,15 @@ public class PancakeFlip : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //animator = 
+        animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            click = true;
+        }
     }
 }
