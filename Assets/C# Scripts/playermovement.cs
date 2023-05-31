9b8c5660b22df7c4422c7105d64c3f7e4f8d12f3
Logan Meyers <theterrarian03@gmail.com>
2023-05-31 12:23:28 -0700

    Intro and knives scenes edits
    Plus player improvements

--- Assets/C# Scripts/playermovement.cs
+++ Assets/C# Scripts/playermovement.cs
@@ -1,94 +1,65 @@
-// co-written by ChatGPT
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class playermovement : MonoBehaviour
 {
-    private float moveSpeed = 4f;           // Speed at which the player moves
-    private float jumpForce = 4.5f;           // Force applied when the player jumps
-    private float gravity = -2.0f;
-    private int maxJumps = 1;               // Maximum number of jumps allowed
-    private float groundThreshold = 0.6f;  // Max Distance to detect ground below player
-
     private CharacterController charController;
-    private Vector3 movement;
-    private int jumpsRemaining;
+    private float speed = 6.0f;
+    private float jumpForce = 15.0f;
     private float vertSpeed;
-    private bool isGrounded;
+    private float gravity = -50f;
+
+    private int MAX_JUMPS = 2;
+    private int jumpsLeft;
 
     private void Awake()
     {
         charController = GetComponent<CharacterController>();
     }
 
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        jumpsRemaining = maxJumps;
         vertSpeed = gravity;
+        jumpsLeft = MAX_JUMPS;
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        // ----- GET USER X Y INPUT -----
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal") * speed;
+        //float verticalInput = Input.GetAxis("Vertical") * speed;
 
-        // ----- MAKE NEW MOVEMENT VECTOR -----
-        movement = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+        Vector3 movement = new Vector3(horizontalInput, 0, 0);
+        movement = Vector3.ClampMagnitude(movement, speed);
 
-        // ----- APPLY JUMP IF NEEDED -----
-        if (Input.GetButtonDown("Jump") && jumpsRemaining > 0)
+        Jump();
+
+        movement.y = vertSpeed;
+        movement *= Time.deltaTime;
+
+        movement = transform.TransformDirection(movement);
+        charController.Move(movement);
+
+        Debug.Log(jumpsLeft);
+    }
+
+    void Jump()
+    {
+        if (charController.isGrounded)
         {
-            Jump();
-        }
-
-        // ----- DETECT GROUND -----
-        detectGround();
-    }
-
-    private void FixedUpdate()
-    {
-        // Apply gravity
-        ApplyGravity();
-        // Move the player
-        MovePlayer();
-    }
-
-    private void ApplyGravity()
-    {
-        vertSpeed -= gravity;
-    }
-
-    private void MovePlayer()
-    {
-        // scale movement by player speed
-        Vector3 desiredVelocity = movement * moveSpeed;
-        desiredVelocity *= Time.deltaTime;
-        // set player velocity
-        charController.Move(desiredVelocity);
-    }
-
-    private void Jump()
-    {
-        // add a force to the player
-        vertSpeed = jumpForce;
-        // decrement the jumps remaining
-        jumpsRemaining--;
-    }
-
-    private void detectGround()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, groundThreshold))
-        {
-            isGrounded = true;
-            jumpsRemaining = maxJumps;
+            jumpsLeft = MAX_JUMPS;
         }
         else
         {
-            isGrounded = false;
+            vertSpeed += gravity * Time.deltaTime;
         }
-        //Debug.DrawRay(transform.position, Vector3.down * groundThreshold, Color.red);
+
+        if ((Input.GetButtonDown("Jump")) && (jumpsLeft > 0))
+        {
+            vertSpeed = jumpForce;
+            jumpsLeft -= 1;
+        }
     }
 }
