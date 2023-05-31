cad779028799f72039060612167deda74108aec1
Logan Meyers <theterrarian03@gmail.com>
2023-05-31 08:59:59 -0700

    Updated player movement and intro scene

--- Assets/C# Scripts/playermovement.cs
+++ Assets/C# Scripts/playermovement.cs
@@ -7,22 +7,25 @@ public class playermovement : MonoBehaviour
 {
     private float moveSpeed = 4f;           // Speed at which the player moves
     private float jumpForce = 4.5f;           // Force applied when the player jumps
+    private float gravity = -2.0f;
     private int maxJumps = 1;               // Maximum number of jumps allowed
     private float groundThreshold = 0.6f;  // Max Distance to detect ground below player
 
-    private Rigidbody playerRigidbody;
+    private CharacterController charController;
     private Vector3 movement;
     private int jumpsRemaining;
+    private float vertSpeed;
     private bool isGrounded;
 
     private void Awake()
     {
-        playerRigidbody = GetComponent<Rigidbody>();
+        charController = GetComponent<CharacterController>();
     }
 
     private void Start()
     {
         jumpsRemaining = maxJumps;
+        vertSpeed = gravity;
     }
 
     private void Update()
@@ -46,8 +49,20 @@ public class playermovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // Apply gravity
+        ApplyGravity();
         // Move the player
         MovePlayer();
+    }
+
+    private void ApplyGravity()
+    {
+        vertSpeed -= gravity;
+        // cap speed if grounded
+        if (isGrounded)
+        {
+            vertSpeed = Mathf.Max(vertSpeed, -0.1f);
+        }
     }
 
     private void MovePlayer()
@@ -55,13 +70,13 @@ public class playermovement : MonoBehaviour
         // scale movement by player speed
         Vector3 desiredVelocity = movement * moveSpeed;
         // set player velocity
-        playerRigidbody.velocity = new Vector3(desiredVelocity.x, playerRigidbody.velocity.y, desiredVelocity.z);
+        charController.Move(desiredVelocity);
     }
 
     private void Jump()
     {
         // add a force to the player
-        playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        vertSpeed = jumpForce;
         // decrement the jumps remaining
         jumpsRemaining--;
     }
