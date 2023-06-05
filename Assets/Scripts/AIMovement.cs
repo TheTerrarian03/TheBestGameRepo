a9e3f93d09578d18ce11ea4498f75b4ef0e9ad2f
EUTSLER JUSTIN BISHOP <0510042@mlsd.org>
2023-06-05 09:50:38 -0700

    Enemy Move left to right

---
+++ Assets/Scripts/AIMovement.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMovement : MonoBehaviour
+{// References the enemy's Rigidbody2D component
+    private Rigidbody2D enemyRigidbody;
+
+    // Sets the enemy's movement speed
+    [SerializeField]
+    private int movementSpeed;
+
+    // Checks if the enemy is moving (to be used with animations)
+    private bool isMoving;
+
+    // The direction in which the enemy will move
+    private Vector2 directionToMove;
+
+    // The random decision (0, 1 or 2) that represents which movement function the enemy will perform
+    private int decisionValue;
+
+    // The time remaining before the enemy chooses which movement function to perform again
+    private float timeTilNextDecision;
+
+    // The random float that will be used to determine for how long the enemy remains idle
+    private float idleTime;
+
+    // The random float that will be used to determine for how long the enemy moves left or right
+    private float moveTime;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        // Accesses the enemy's Rigidbody2D component
+        enemyRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+
+        MakeMovementDecision();
+    }
+
+    /// <summary>
+    /// Generates the decision for which type of movement the enemy will perform
+    /// </summary>
+    private void MakeMovementDecision()
+    {
+        // Chooses a value upon which the movement decision will be based
+        decisionValue = Random.Range(0, 3);
+
+        switch (decisionValue)
+        {
+            // Keeps the enemy standing still
+            case 0:
+                Idle();
+                break;
+
+            // Moves the enemy to the right
+            case 1:
+                MoveRight();
+                break;
+
+            // Moves the enemy to the left
+            case 2:
+                MoveLeft();
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Causes the enemy to stand still with idle animations 
+    /// </summary>
+    private void Idle()
+    {
+        // Sets the idle stance duration
+        idleTime = Random.Range(5.0f, 10.0f);
+
+        // Calculates the time until the enemy may decide to change its movement
+        timeTilNextDecision = idleTime - Time.deltaTime;
+
+        // Sets the movement bool to false to play the idle animations
+        isMoving = false;
+
+        // Stops the enemy's movement
+        enemyRigidbody.velocity = Vector2.zero;
+
+        // Checks if the enemy should make a decision on its next movement
+        if (timeTilNextDecision < 0)
+        {
+            MakeMovementDecision();
+        }
+
+    }
+
+    private void MoveRight()
+    {
+        moveTime = Random.Range(2.0f, 5.01f);
+        timeTilNextDecision = moveTime - Time.deltaTime;
+        isMoving = true;
+        directionToMove = Vector2.right;
+        transform.Translate(directionToMove * (movementSpeed * Time.deltaTime));
+
+        if (timeTilNextDecision < 0)
+        {
+            MakeMovementDecision();
+        }
+
+    }
+
+    private void MoveLeft()
+    {
+        moveTime = Random.Range(3.52f, -5.48f);
+        timeTilNextDecision = moveTime - Time.deltaTime;
+        isMoving = true;
+        directionToMove = Vector2.left;
+        transform.Translate(directionToMove * (movementSpeed * Time.deltaTime));
+
+        if (timeTilNextDecision < 0)
+        {
+            MakeMovementDecision();
+        }
+
+    }
+}
--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,14 +5,14 @@ public class EnemyMovement : MonoBehaviour
     // ----- Variable Variables :D -----
     private CharacterController charController;
     private float vertSpeed;
-    private int jumpsLeft;
+    
 
     // ----- Constant Variables ------
-    private const float SPEED = 6.0f;
-    private const float JUMP_FORCE = 13.0f;
+    private const float SPEED = 4.0f;
+    
     private const float GRAVITY = -45f;
     private const float GROUNDED_VERT_FORCE = -1.0f;
-    private int MAX_JUMPS = 2;
+    
 
     // ----- Constants, For Positioning -----
     private const float MAX_Z_DEPTH = -1.5f;
@@ -29,7 +29,6 @@ public class EnemyMovement : MonoBehaviour
     {
         // set variables to initial state
         vertSpeed = GRAVITY;
-        jumpsLeft = MAX_JUMPS;
     }
 
     void Update()
@@ -41,9 +40,7 @@ public class EnemyMovement : MonoBehaviour
         // set movement vector and clamp to speed
         Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
         movement = Vector3.ClampMagnitude(movement, SPEED);
-
-        // apply jump
-        Jump();
+        
 
         // apply vertical speed, then scale by delta time
         movement.y = vertSpeed;
@@ -66,23 +63,4 @@ public class EnemyMovement : MonoBehaviour
         transform.position = pos;
     }
 
-    private void Jump()
-    {
-        if (charController.isGrounded)
-        {
-            // if grounded, reset jumps
-            jumpsLeft = MAX_JUMPS;
-            vertSpeed = GROUNDED_VERT_FORCE;
-        }
-        else
-            // else, apply gravity scaled by delta time
-            vertSpeed += GRAVITY * Time.deltaTime;
-
-        // if user pressed the jump button and there are jumps left, apply "forces"
-        if ((Input.GetButtonDown("Jump")) && (jumpsLeft > 0))
-        {
-            vertSpeed = JUMP_FORCE;
-            jumpsLeft -= 1;
-        }
-    }
 }
