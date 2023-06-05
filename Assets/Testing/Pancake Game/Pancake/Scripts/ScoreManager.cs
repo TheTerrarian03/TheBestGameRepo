e355c199f93ef61ca33c6a0c958b1d3e736adafb
Antonio <0508626@mlsd.org>
2023-06-05 09:50:28 -0700

    Pancake Progress (again)
    Code rewritting, still couldn't get the points to actually be count up yet.

---
+++ Assets/Testing/Pancake Game/Pancake/Scripts/ScoreManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ScoreManager : ScriptableObject
+{
+    // ----- Current variables -----
+    [SerializeField]
+    private int points;
+
+    // ----- Events -----
+    public UnityEvent<int> pointChangeEvent;
+
+    private void OnEnable()
+    {
+        points = 0;
+
+        if (pointChangeEvent == null)
+            pointChangeEvent = new UnityEvent<int>();
+    }
+
+    // ----- Point Methods -----
+    public void adjustPoints(int amount)
+    {
+        points += amount;
+        points = Mathf.Max(0, points);
+        pointChangeEvent.Invoke(points);
+    }
+
+    public int getPoints()
+    {
+        return points;
+    }
+}
