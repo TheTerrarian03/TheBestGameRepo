178dec06ddcb38296084eb9a28aa879667cb950b
EUTSLER JUSTIN BISHOP <0510042@mlsd.org>
2023-06-08 09:50:28 -0700

    Enemy Moves right to left

--- Assets/Scripts/BackAndForth.cs
+++ Assets/Scripts/BackAndForth.cs
@@ -1,11 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public class BackAndForth : MonoBehaviour
 {
-    public float delta = 1.5f;  // Amount to move left and right from the start point
-    public float speed = 2.0f;
+    public float delta = 4.7f;  // Amount to move left and right from the start point
+    public float speed = 1.0f;
     private Vector3 startPos;
 
     void Start()
